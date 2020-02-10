8f130d4639d9cae9c0829014b1ddfff524aa5707
Mustafa Günel <34231087+gunel4755@users.noreply.github.com>
2020-02-11 00:07:23 +0300

    Update insertion sort.cpp

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -36,17 +36,8 @@ main(){
 	for(int m=0; m <= n ; m++){
 		printf("%d, ", a[m]);
 	}
-    
-    /*for (i = 1; i < n; i++) { 
-        key = arr[i]; 
-        j = i - 1; 
-  
-        while (j >= 0 && arr[j] > key) { 
-            arr[j + 1] = arr[j]; 
-            j = j - 1; 
-        } 
-        arr[j + 1] = key; 
-    } */
+
+	
 
 	
 			
